2bc3a9a5145813b05e1f2afba10f0871aeb39b9e
Daniel Walter <realdanielwalter@gmail.com>
2020-11-17 21:14:35 -0800

    I changed env mode in nuxt config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   apps : [{
     script: './node_modules/nuxt/bin/nuxt.js',
+    node_args : '-r dotenv/config NODE_ENV=production',
     watch: '.',
     name: "cudua-commerce-frontend",
     exec_mode: "fork",
