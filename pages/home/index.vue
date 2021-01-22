0ae348c1d266b626b593c5dd85513d93045d7cb3
Daniel Walter <realdanielwalter@gmail.com>
2021-01-22 13:47:17 -0800

    i started landing page design

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,27 +1,50 @@
 <template>
   <div class="customer">
     <div class="page-container">
-        <div class="position-relative">
-            <div class="hero-section-background">
-                <div class="circle-section"></div>
-                <div class="wave-section"></div>
-                <div class="circle-1"></div>
-                <div class="circle-2"></div>
+      <!-- section one -->
+      <div class="banner-area">
+        <div class="menu-area">
+            <div class="banner-logo">
+              <img src="~/assets/global-asset/image/cudua-logo-full.png" alt="" srcset="">
             </div>
-            <div class="hero-content-area">
-              <div class="hero-nav-bar">
-                  <n-link to="/" class="site-footer-logo">
-                      <img src="~/assets/customer/image/cudua-white.svg">
-                  </n-link>
-                  <!-- <a href="#" class="btn btn-primary">Create your shop</a> -->
-              </div>
+            <div class="">
 
-              <div class="intro-text">
-                
-              </div>
             </div>
         </div>
+        <div>
+
+          <div class="banner-text">
+            <div class="big-header">
+              Move your <span>fashion</span> & <span>Beauty</span> business online in 2 minutes.
+            </div>
+            <div class="intro-text">
+              <p>Create a beautiful online store and gain access to all the tools you will need to turbo charge your business growth online for free.</p>
+            </div>
+
+            <div class="take-banner-action">
+              <n-link to="/auth/create-store" class="btn btn-primary">Sign up now - it's free</n-link>
+              <n-link to="" class="">See an example</n-link>
+            </div>
+            
+          </div>
+
+          <div class="banner-image-container">
+            <div class="floating-div float-blue"></div>
+            <div class="floating-div float-yellow"></div>
+            <div class="floating-div float-red"></div>
+            <div class="banner-image">
+              <img src="~/assets/global-asset/image/screenshots/store-front.png" alt="" srcset="">
+            </div>
+          </div>
+
+        </div>
+
       </div>
+
+      <!-- section-two -->
+      <div class="section-container"></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
