f34eafeb6097151c581b5144a53595c4e37a29dd
Daniel Walter <realdanielwalter@gmail.com>
2021-01-27 04:07:43 -0800

    I modified landing page

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,6 +3,7 @@
     <div class="page-container">
       <!-- section one -->
       <div class="banner-area">
+        <div class="float-container"></div>
         <div class="menu-area">
             <div class="banner-logo">
               <img src="~/assets/global-asset/image/cudua-logo-full.png" alt="" srcset="">
@@ -15,10 +16,10 @@
 
           <div class="banner-text">
             <div class="big-header">
-              Move your <span>fashion</span> & <span>Beauty</span> business online in 2 minutes.
+                Where fashion and beauty businesses sell their products.
             </div>
             <div class="intro-text">
-              <p>Create a beautiful online store and gain access to all the tools you will need to turbo charge your business growth online for free.</p>
+              <p>Create a beautiful online store and gain access to all the tools you will need to turbo charge your business growth and sell online for free.</p>
             </div>
 
             <div class="take-banner-action">
@@ -29,11 +30,8 @@
           </div>
 
           <div class="banner-image-container">
-            <div class="floating-div float-blue"></div>
-            <div class="floating-div float-yellow"></div>
-            <div class="floating-div float-red"></div>
             <div class="banner-image">
-              <img src="~/assets/global-asset/image/screenshots/store-front.png" alt="" srcset="">
+              <img src="~/assets/global-asset/image/screenshots/banner.png" alt="" srcset="">
             </div>
           </div>
 
@@ -42,8 +40,29 @@
       </div>
 
       <!-- section-two -->
-      <div class="section-container"></div>
-      <div></div>
+      <div class="section-container section-background">
+          <div class="section-header-text">Great for customers, Great for your business.</div>
+          <div class="sub-section-text mg-bottom-32">Cudua provides customers and businesses a modern, efficient, and yet simple tools to buy and sell products with ease.</div>
+          <div class="banner-image d-flex-center">
+            <img src="~/assets/global-asset/image/screenshots/banner2.png" alt="" srcset="">
+          </div>
+      </div>
+      <div class="section-container">
+        <div class="">
+          <div class="features-header">Be in charge of your store online.</div>
+          <div class="features-body">
+            Take total control of your store online with the easy-to-use tools that are available in your <span>SHOP MANAGER</span> to manage day-to-day online activities.
+          </div>
+          <div>
+            
+          </div>
+
+          <div class="product-benefit-images">
+            <img src="~/assets/global-asset/image/screenshots/total-control.png" alt="" srcset="">
+          </div>
+
+        </div>
+      </div>
     </div>
   </div>
 </template>
